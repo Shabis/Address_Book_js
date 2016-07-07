f96aa7ff9619c8ffacd082de1d9f4e42b1e57bfb
Shelby Clayton <shelby_clayton@hotmail.com>
2016-07-06 20:11:25 -0700

    add functionality to form in index.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//business logic
 function Contact(first, last, street, city, state) {
   this.firstName = first;
   this.lastName = last;
@@ -5,3 +6,22 @@ function Contact(first, last, street, city, state) {
   this.city = city;
   this.state = state;
 }
+
+//user-interface logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    var firstNameInput = $("input#new-first-name").val();
+    var lastNameInput = $("input#new-last-name").val();
+    var streetNameInput = $("input#new-street-name").val();
+    var cityNameInput = $("input#new-city-name").val();
+    var stateNameInput = $("input#new-state-name").val();
+
+    var newContact = new Contact(firstNameInput, lastNameInput, streetNameInput, cityNameInput, stateNameInput);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
